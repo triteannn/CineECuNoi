364e2f5872b19ea53c288be713723a60967f9df8
Adrian <as24ady@gmail.com>
2018-03-21 13:24:20 +0200

    label

--- ISSApp/ISSApp/Form1.Designer.cs
+++ ISSApp/ISSApp/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(496, 453);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1450, 1078);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
