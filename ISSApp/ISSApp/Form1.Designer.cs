42f34fe157ef2c31fb366faaf198eb84da51c556
triteannn <tudortritean@gmail.com>
2018-03-21 13:54:19 +0200

    Am reparat label

--- ISSApp/ISSApp/Form1.Designer.cs
+++ ISSApp/ISSApp/Form1.Designer.cs
@@ -28,23 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(496, 453);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Adi";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.BtnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BtnExit
@@ -57,17 +42,30 @@
             this.BtnExit.UseVisualStyleBackColor = true;
             this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Adi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(796, 446);
             this.ControlBox = false;
             this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
