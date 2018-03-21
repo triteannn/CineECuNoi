532f70ccb879b80cd19b191e9b5b7b2b5d4f6c3e
Adrian <as24ady@gmail.com>
2018-03-21 13:55:38 +0200

    test

--- ISSApp/ISSApp/Form1.Designer.cs
+++ ISSApp/ISSApp/Form1.Designer.cs
@@ -29,29 +29,25 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.BtnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(496, 453);
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(760, 348);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Adi";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
-            this.BtnExit = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
             // BtnExit
             // 
-            this.BtnExit.Location = new System.Drawing.Point(713, 415);
+            this.BtnExit.Location = new System.Drawing.Point(725, 348);
             this.BtnExit.Name = "BtnExit";
-            this.BtnExit.Size = new System.Drawing.Size(75, 23);
+            this.BtnExit.Size = new System.Drawing.Size(138, 73);
             this.BtnExit.TabIndex = 0;
             this.BtnExit.Text = "Exit";
             this.BtnExit.UseVisualStyleBackColor = true;
@@ -59,9 +55,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2086, 1166);
             this.ControlBox = false;
             this.Controls.Add(this.BtnExit);
             this.DoubleBuffered = true;
