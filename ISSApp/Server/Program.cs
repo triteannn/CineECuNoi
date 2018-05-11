2631972abad31e1b975447c106a9bec431770808
AdrianFaur <adrianfaur1997@gmail.com>
2018-05-11 19:22:01 +0300

    Added Server Main, server is considered working until the implementation of the client

--- ISSApp/Server/Program.cs
+++ ISSApp/Server/Program.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading.Tasks;
+using Server.Networking;
 
 namespace Server
 {
@@ -10,6 +18,34 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
+            serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+            BinaryClientFormatterSinkProvider clientProv = new BinaryClientFormatterSinkProvider();
+            IDictionary props = new Hashtable();
+
+            props["port"] = int.Parse(ConfigurationManager.AppSettings["Port"]);
+
+            TcpChannel channel = new TcpChannel(props, clientProv, serverProv);
+            ChannelServices.RegisterChannel(channel, false);
+
+            var service = new ServerImpl();
+
+            RemotingServices.Marshal(service, "DonareSangeISS");
+
+            Console.WriteLine("Server is up on " + GetLocalIPAddress() + " on port: " + props["port"]);
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
 }
