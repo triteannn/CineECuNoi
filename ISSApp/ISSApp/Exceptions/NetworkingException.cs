e8d7795ac6e00415270350317e2264e24869fe7a
AdrianFaur <adrianfaur1997@gmail.com>
2018-05-11 15:32:25 +0300

    Added Exceptions package and created  custom exceptions for repositories and networking

---
+++ ISSApp/ISSApp/Exceptions/NetworkingException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISSApp.Exceptions
+{
+    class NetworkingException : Exception
+    {
+        public NetworkingException(string message) :base(message)
+        {
+
+        }
+    }
+}
