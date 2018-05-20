f47515fa85250bea2922106c433fb3e66ede26b0
AdrianFaur <adrianfaur1997@gmail.com>
2018-05-20 21:03:52 +0300

    Moved Exception, used it in the Service.

--- ISSApp/ISSApp/Exceptions/SeviceException.cs
+++ ISSApp/ISSApp/Exceptions/SeviceException.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Client.Exceptions
+namespace ISSApp.Exceptions
 {
-    class SeviceException: Exception
+    public class SeviceException: Exception
     {
         public SeviceException(string message) : base(message)
         {
