6e9c52e4eda75b2ad5870264fa3dee3fe27c4030
Adrian <as24ady@gmail.com>
2018-05-20 12:23:00 +0300

    Initializare ServiceAnaliza

---
+++ ISSApp/ISSApp/Service/ServiceAnaliza.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISSApp.Domain;
+using ISSApp.Networking;
+
+namespace ISSApp.Service
+{
+    class ServiceAnaliza : IService
+    {
+        private readonly IServer _server;
+
+        public ServiceAnaliza(IServer server)
+        {
+            _server = server;
+        }
+
+
+    }
+}
