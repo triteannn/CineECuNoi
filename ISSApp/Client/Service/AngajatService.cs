4675e8db3694ab68d32071f2f638ed12b6701453
Adrian <as24ady@gmail.com>
2018-05-30 17:54:10 +0300

    AngajatService, MedicService

---
+++ ISSApp/Client/Service/AngajatService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISSApp.Networking;
+
+namespace Client.Service
+{
+    class AngajatService
+    {
+        private IServer _server;
+
+        public AngajatService(IServer server)
+        {
+            _server = server;
+        }
+    }
+}
