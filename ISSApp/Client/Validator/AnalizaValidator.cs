b4c1c7fedfbe0ec0cfc1a27dcad8a8a7ae9631a3
miat96 <trifa_maria96@yahoo.com>
2018-06-05 23:51:50 +0300

    Analiza validator

---
+++ ISSApp/Client/Validator/AnalizaValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ISSApp.Exceptions;
+
+namespace Client.Validator
+{
+    public static class AnalizaValidator
+    {
+
+        //se apeleaza pentru fiecare valoare
+        public static void ValidareRezultat(string rezultat)
+        {
+            var regex = new Regex("^\\-?[0-9]{1,8}(\\.[0-9]{1,8})?$");
+            if (!regex.IsMatch(rezultat))
+                throw new ValidationException("Rezultat invalid.");
+        }
+    }
+}
